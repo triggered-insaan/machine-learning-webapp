159b413d3d1f5008d7a24677594c0a7ef6640e7f
username <masterofshadows6053@gmail.com>
2023-10-20 15:36:51 +0530

    Page updated

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route('/', methods=['get','post'])
 def home():
-    return render_template('send.html')
+    return render_template('index.html')
 
 from controller import *
